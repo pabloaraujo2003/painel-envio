df23d17b2a1ef04e571e8ea559d8d1c26f780661
pabloaraujo2003 <pablovitoraraujoazevedo@outlook.com>
2025-12-16 15:14:00 -0300

    git pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/painel-envio/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3001"
-    }
-  }
 });
-//export default App
